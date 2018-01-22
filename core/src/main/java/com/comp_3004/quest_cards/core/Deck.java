07145f9098c2832e78b4429f1c7ca47ec751f959
Frerik Drumm <frerikdrumm@cmail.carleton.ca>
2018-01-22 14:59:03 -0500

    welcome to the start of the new card-deck implementation development branch!!!

--- core/src/main/java/com/comp_3004/quest_cards/core/Deck.java
+++ core/src/main/java/com/comp_3004/quest_cards/core/Deck.java
@@ -1,7 +1,7 @@
 package com.comp_3004.quest_cards.core;
 
 import java.util.Stack;
-
+//welcome to the start of the new card-deck implementation development branch!!!
 public abstract class Deck {
 	
 	protected Stack<Card> deck;
