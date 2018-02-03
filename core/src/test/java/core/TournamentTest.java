e669ebfded8de033a262b31ef887f730704db1dd
Patrick Dziemian <patrickdziemian@cmail.carleton.ca>
2018-02-02 19:52:50 -0500

    Current Tournament Tests: test players setting their participation, setTournamentParticupation, F6 2018-02-02 test having too many cards joining tournament, and discarding a card to have valid hand, F6 2018-02-02 test Match->playCard tested playing Weapon card during users turn, success card added, Test 3, F6 2018-02-02

--- core/src/test/java/core/TournamentTest.java
+++ core/src/test/java/core/TournamentTest.java
@@ -1,7 +1,25 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+import com.badlogic.gdx.Game;
+import com.comp_3004.quest_cards.cards.AdventureCard;
+import com.comp_3004.quest_cards.cards.TournamentCard;
+import com.comp_3004.quest_cards.cards.WeaponCard;
+import com.comp_3004.quest_cards.core.GameModel;
+import com.comp_3004.quest_cards.core.Player;
+import com.comp_3004.quest_cards.core.Players;
+import com.comp_3004.quest_cards.core.Tournament;
+import com.comp_3004.quest_cards.core.GameModel.cardModes;
+
+import junit.framework.TestCase;
 
 public class TournamentTest extends TestCase{
+	/*
 	public void testTournament1() {
 		//testing players setting their participation, setTournamentParticupation
 		int numPlayers = 4;
@@ -60,6 +78,55 @@ public class TournamentTest extends TestCase{
 		assertEquals(false, player.tooManyHandCards()); //discarded correct number now
 		game.done(); //done turn	
 	}
+	*/ 
+	public void testTournament3() {
+		// testing playCard() Playing a Weapon Card during turn, check was added correctly to right player
+		int time = 100;
+		//Test Tournament joiners having too many cards, testing discardCardIfTooMany()
+		int numPlayers = 3;
+		final GameModel game = new GameModel();
+		game.initPlayersStart(numPlayers);
+		
+		Player p3 = new Player("Player 3");
+		LinkedList<AdventureCard> cards = new LinkedList<AdventureCard>();
+		p3.participateTour(true);
+		WeaponCard excalibur = new WeaponCard("Excalibur", 30);
+		cards.add(excalibur);
+		p3.setHand(cards);
+		game.addPlayer(p3);
+		
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+			game.startGame();
+			}
+		});
+		thread.start();
+		
+		sleep(time);
+		game.setParticipation(false); // p0 no
+		sleep(time);
+		game.setParticipation(false);//p1 no
+		sleep(time);
+		game.setParticipation(true);  //p2 yes  player has to many cards by 1 //game blocked until discarded
+		sleep(time);
+		game.cardPressed(0);
+		game.done(); //done turn
+		sleep(time);
+		game.setParticipation(true);  //p3 yes good hand <=12
+		sleep(time);
+		//now player2's turn
+		game.cardPressed(0);
+		game.done();
+		sleep(time);
+		Player curr = game.getMatch().getPlayers().current();
+		game.cardPressed(0);
+		sleep(time);
+		LinkedList<AdventureCard> cards2 = curr.getActive();
+		boolean r = (AdventureCard)cards2.get(0) == excalibur;
+		assertEquals(true, r);		
+	}
+	
 	public void sleep(int milisecs) {
 		try {
 			Thread.sleep(milisecs); // sleeping to wait for game initialization
