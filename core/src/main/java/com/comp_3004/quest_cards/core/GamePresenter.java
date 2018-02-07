82148bcf22491bda2287693122b81552a8bc1c0a
Frerik Drumm <frerikdrumm@cmail.carleton.ca>
2018-02-06 19:33:15 -0500

    created a rough listener template in the presenter

--- core/src/main/java/com/comp_3004/quest_cards/core/GamePresenter.java
+++ core/src/main/java/com/comp_3004/quest_cards/core/GamePresenter.java
@@ -1,20 +1,9 @@
 package com.comp_3004.quest_cards.core;
 
-
-import java.util.LinkedList;
-import java.util.Stack;
-
 import org.apache.log4j.Logger;
-
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.comp_3004.quest_cards.cards.AdventureCard;
-import com.comp_3004.quest_cards.cards.AdventureDeck;
-import com.comp_3004.quest_cards.cards.StoryCard;
-import com.comp_3004.quest_cards.cards.StoryDeck;
-import com.comp_3004.quest_cards.core.Player.Rank;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.DragListener;
 import com.comp_3004.quest_cards.gui.GameScreen;
 
 
@@ -28,54 +17,40 @@ public class GamePresenter extends Group{
 		this.view = v;
 		addActor(view);
 		this.model = m;
-	}
-	
-	//send model data to the view
-	private Rank setViewRank(int i) { 
-		return model.getPlayerAtIndex(i).getRank();
-		//GameScreen.methodUpdateRelvantInformation().
-	}
-	private int setViewShields(int i) { return model.getPlayerAtIndex(i).getShields(); }
-	private LinkedList<AdventureCard> setViewHand(int i) { return model.getPlayerAtIndex(i).getHand(); }
-	private LinkedList<AdventureCard> setViewActive(int i) { return model.getPlayerAtIndex(i).getActive(); }
-	private Stack<AdventureCard> setViewAdvDeck() { return model.getAdvDeck().getDeck(); }
-	private Stack<AdventureCard> setViewAdvDiscard() { return model.getAdvDeck().getDiscard(); }
-	private Stack<StoryCard> setViewStoryDeck() { return model.getStoryDeck().getDeck(); }
-	private Stack<StoryCard> setViewStoryDiscard() { return model.getStoryDeck().getDiscard(); }
-	
-	//perform actions from view on model
-	public void startGame() {	//user presses new game on main menu
-		model = new GameModel();
-	}
-	public void playerPlaysAdventureCard(int i, AdventureCard card) {		//user drags card from hand to play
-		model.getPlayerAtIndex(i).playCard(card);
-	}
-	public void playerDiscardsAdventureCard(int i, AdventureCard card) {	//user drags card from hand to discard
-		model.getPlayerAtIndex(i).discardCard(card, model.getAdvDeck());
-	}
-	
-	public void draw(Batch batch, float parentAlpha) {
-		for(int i=0; i<model.getNumPlayers(); i++) {
-			setViewRank(i);
-			setViewShields(i);
-			setViewHand(i);
-			setViewActive(i);
-		}
-		setViewAdvDeck();
-		setViewAdvDiscard();
-		setViewStoryDeck();
-		setViewStoryDiscard();
 		
-		view.draw(batch, parentAlpha);
-	}
-	
-	//overwrite
-	public void act() {
 		
+		//this is just a rough idea of what a listener may look like...
+		//play card from hand listener
+		view.addListener(new DragListener() {
+			//user clicks on a card and drags it
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				/*if x,y are inside a cards tap square
+					get card ID
+					return true;
+				else x,y are not inside a cards tap square*/
+				return false;
+			}
+			//user drags card to play and releases it
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				/*if x,y are inside the play area's tap square
+					if (model.getcurrentTurn().playCard(card)) {
+						card played successfully (card was allowed to be played by model)
+						these next two methods are essentially setters for view attributes
+						view.updateHand(model.getcurrentTurn().getHand());
+						view.updateActive(model.getcurrentTurn().getActive());
+					}*/
+			}
+		});
 	}
 	
-	
-	
-	
-	
+	//won't need these, as the calls to model.getSomething can be placed directly in the listeners
+	/*update view with model data - called when a change in model occurs
+	private Rank updateRank() { return model.getcurrentTurn().getRank(); }
+	private int updateShields() { return model.getcurrentTurn().getShields(); }
+	private LinkedList<AdventureCard> updateHand() { return model.getcurrentTurn().getHand(); }
+	private LinkedList<AdventureCard> updateActive() { return model.getcurrentTurn().getActive(); }
+	private Stack<AdventureCard> updateAdvDeck() { return model.getAdvDeck().getDeck(); }
+	private Stack<AdventureCard> updateAdvDiscard() { return model.getAdvDeck().getDiscard(); }
+	private Stack<StoryCard> updateStoryDeck() { return model.getStoryDeck().getDeck(); }
+	private Stack<StoryCard> updateStoryDiscard() { return model.getStoryDeck().getDiscard(); }*/
 }
