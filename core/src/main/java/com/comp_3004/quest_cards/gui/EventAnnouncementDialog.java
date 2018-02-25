a48c63514c5d561d18e5a490dc499b35c6d04210
Kevin2kp <Kevin2kp@gmail.com>
2018-02-25 00:02:36 -0500

    Added event dialogs

---
+++ core/src/main/java/com/comp_3004/quest_cards/gui/EventAnnouncementDialog.java
@@ -0,0 +1,39 @@
+package com.comp_3004.quest_cards.gui;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class EventAnnouncementDialog extends AnnouncementDialog {
+  public EventAnnouncementDialog(Skin skin) {
+    super(skin);
+    super.setBounds(1920 / 2- WIDTH / 2, 1080 / 2- HEIGHT / 2, WIDTH, HEIGHT);
+  }
+
+  private static final int WIDTH = 400;
+  private static final int HEIGHT = 600;
+
+  private CardView cardView;
+
+  @Override
+  public void setTitle(String title){
+
+  }
+
+  @Override
+  public void setBounds(float x, float y, float width, float height) {
+  }
+
+  public void setCardView(CardView cardView) {
+    this.cardView = cardView;
+  }
+
+  @Override
+  public void draw(Batch batch, float alpha) {
+
+    super.draw(batch, alpha);
+
+    if (cardView != null) {
+      cardView.getDrawable().draw(batch, 1920 / 2 - 366 / 2, 1080 / 2 - 500 / 2 + 25, 366, 500);
+    }
+  }
+}
